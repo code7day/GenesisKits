80eeac94b523c2b04c31c40bd8f764ae62de4383
Edu. Flores <zedu77@gmail.com>
2026-01-30 13:00:47 -0500

    fix

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,14 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-<<<<<<< HEAD
-    App\Providers\Filament\DashboardPanelProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
-=======
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
->>>>>>> 286a935 (fix)
 ];
